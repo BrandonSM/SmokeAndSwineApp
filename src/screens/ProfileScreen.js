5ea9d83f076fa44866065c180a41f7f0440f1aaa
Brandon McGinnis <brandon@Brandons-MacBook-Pro.local>
2021-01-20 18:43:37 -0500

    commit before Firebase

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,33 +1,11 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Text, View } from 'react-native';
 import PaymentsScreen from './PaymentsScreen';
 
+
 export default function ProfileScreen({navigation}) {
-
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  // Handle user state changes
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
-
-  if (initializing) return null;
-
-  if (!user) {
-    return (
-      <View>
-        <Text>c</Text>
-      </View>
-    );
-  }
 
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
